61b12c4061b639d0ec386ca6aa9b2997a56e047b
Bruno S Marques <brunosmarques1995@hotmail.com>
2016-09-09 22:10:46 -0300

    Formulários + Criptografia em PHP

---
+++ teste.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <p>
+        <?php
+            if(filter_has_var(0, "envio"))
+                {
+                $nome = filter_input(0, "usu", 516);
+                $senha = hash("sha256",filter_input(0, 'senha', 516));
+                if($nome==NULL)
+                    {
+                    printf("Campo obrigatório\n");
+                    }
+                else
+                    {
+                    printf("Olá %s de senha %s",$nome,$senha);
+                    }
+                }
+        ?>
+        </p>
+        <a href='index.html'>Voltar</a>
+    </body>
+        
+</html>
