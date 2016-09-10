817a2cdfe26ff38630fd1734384155b3cf50a889
Bruno S Marques <brunosmarques1995@hotmail.com>
2016-09-10 19:22:23 -0300

    Leitura checkbox
    o atributo name de cada checkbox deve ser diferente, já que sua leitura em PHP é independente para cada caixa

--- teste.php
+++ teste.php
@@ -9,10 +9,14 @@
         <?php
             if(filter_has_var(0, "envio"))
                 {
-                $nome = filter_input(0, "usu", 516);
+                $nome = filter_input(0, "usu", 515);
                 $senha_ini = 'd011587559c50660e07819222e94382df01ccc3ac588cc0ace18830a77a7a625c12bcf0635083791118f06f4baef9e15ce8fcbd18817d1d044e220c05a53a3d3';
-                $senha = hash("sha512",filter_input(0, 'senha', 516));
-                $alt = filter_input(0, 'alt', FILTER_SANITIZE_NUMBER_INT);
+                $senha = hash("sha512",filter_input(0, 'senha', 515));
+                $alt = filter_input(0, 'alt', 519);
+                for($i=0;$i<3;++$i)
+                    {
+                    $marca[$i]=  filter_input(0, 'marca'.($i+1),519);
+                    }
                 if($nome==NULL)
                     {
                     printf("Campo obrigatório\n");
@@ -37,7 +41,19 @@
                     {
                     printf("</p>\n<p>Alternativa marcada: %u",$alt);
                     }
-                    fila_pilha();
+                if($marca==NULL)
+                    {
+                    printf("</p>\n<p>Pelo menos um dos campos deve ser preenchido\n");
+                    }
+                else
+                    {
+                    printf("<p>");
+                    for($i=0;$i<3;++$i)
+                        {
+                        printf("%u ",$marca[$i]);
+                        }
+                    printf("</p>");
+                    }
                 }
             function fila_pilha()/*Funcionando corretamente para números*/
                 {
